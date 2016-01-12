df4fa9c32f43942e7ed7a996512b2bf4460fd6b3
Muhammad Umer Farooq <omar.faruk@outlook.com>
2016-01-12 23:19:50 +0000

    Created solution and web project

---
+++ src/Enzyme.Web/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Enzyme.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ src/Enzyme.Web/Services/ISmsSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Enzyme.Web.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
